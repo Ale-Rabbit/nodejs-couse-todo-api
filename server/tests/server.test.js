13525859325dd9df9627de15c46fcef76bbf41dd
Ale-Rabbit <alexandra.peres@hotmail.com>
2018-12-19 16:43:15 -0200

    Adicionando testes para GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 let {app} = require('./../server');
 let {Todo} = require('./../models/todo');
 
+const todos = [{
+    text:'Primeiro teste Todo'
+},{
+    text:'Segundo teste Todo'
+}];
+
 beforeEach((done) => {
-    Todo.deleteMany({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 
@@ -24,8 +32,7 @@ describe('POST /todos', () => {
                 if (err){
                     return done(err);
                 }
-
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -43,11 +50,23 @@ describe('POST /todos', () => {
                 if(err){
                     return done(err);
                 }
+
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => done(e));
             });
-
     });
+
+    describe('GET /todos', () => {
+        it('Deveria funcionar o GET', (done) => {
+            request(app)
+                .get('/todos')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body.todos.length).toBe(2)
+                })
+                .end(done);
+        });
+    })
 });
