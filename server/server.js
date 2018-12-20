5f070aa19cf8cf21719ca7d1bebbe201d7fdd78b
Ale-Rabbit <alexandra.peres@hotmail.com>
2018-12-20 09:03:40 -0200

    Configurando app para Heroku

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
 
 let app = express();
+let port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -49,8 +50,8 @@ app.get('/todos/:id', (req,res) => {
     })
 });
 
-app.listen(3000, () => {
-    console.log('Iniciado na porta 3000');
+app.listen(port, () => {
+    console.log(`Iniciado na porta ${port}`);
 })
 
 module.exports = {app};
